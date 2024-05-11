f92b921a0b0de632a14a7319a0173aa28f6a797f
Alejandra Bonilla <ale.bonilla309@gmail.com>
2024-05-11 20:42:11 +0200

    update

--- Ejercicio1/src/Entrada.java
+++ Ejercicio1/src/Entrada.java
@@ -9,6 +9,11 @@ public class Entrada {
         double suma = 0.0;
         int suspensos = 0;
         int aprobados = 0;
+        boolean maximo = false;
+        boolean minimo = true;
+        double max = 0;
+        double min = 0;
+
 
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 20; i++) {
@@ -16,8 +21,25 @@ public class Entrada {
              double nota = scanner.nextDouble();
             listaAlumnos.add(new Alumno(nota));
             suma += nota;
-            
-             if (nota<5){
+            if (!maximo){
+                max = nota;
+                maximo = true;
+            }else {
+                if (nota > max){
+                    max = nota;
+                }
+            }
+            if (minimo){
+                min = nota;
+                minimo = false;
+            }else {
+                if (nota < min){
+                    min = nota;
+                }
+            }
+
+
+            if (nota<5){
                  suspensos++;
              }
              if (nota>=5){
@@ -26,7 +48,8 @@ public class Entrada {
         }
 
         double media = suma/20;
-
+        System.out.println("La nota maxima es: "+max);
+        System.out.println("La nota minima es: "+min);
         System.out.println("La cantidad de aprovados: "+aprobados);
         System.out.println("La cantidad de suspensos: "+suspensos);
         System.out.println("La nota media: "+media);
